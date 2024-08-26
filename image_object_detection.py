fe4ce00c36cfc83fc7fa427f1671275f81fbfe6a
Ibai Gorordo <43162939+ibaiGorordo@users.noreply.github.com>
2024-08-26 10:49:15 +0900

    Update image_object_detection.py

--- image_object_detection.py
+++ image_object_detection.py
@@ -8,7 +8,7 @@ model_path = "models/yolov10l.onnx"
 detector = YOLOv10(model_path, conf_thres=0.2)
 
 # Read image
-img_url = "https://github.com/ibaiGorordo/ONNX-YOLOv10-MIT-ObjectDetection/releases/download/0.1.0/test.png"
+img_url = "https://github.com/ibaiGorordo/ONNX-YOLOv10-Object-Detection/blob/assets/assets/test.png?raw=true"
 img = imread_from_url(img_url)
 
 # Detect Objects
@@ -18,5 +18,4 @@ class_ids, boxes, confidences = detector(img)
 combined_img = draw_detections(img, boxes, confidences, class_ids)
 cv2.namedWindow("Detected Objects", cv2.WINDOW_NORMAL)
 cv2.imshow("Detected Objects", combined_img)
-cv2.imwrite("doc/img/detected_objects.jpg", combined_img)
 cv2.waitKey(0)
